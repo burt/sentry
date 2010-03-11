1d6ab3ad616a2b348cc407709a0f1e5afbb03ffd
burt <funkingburt@gmail.com>
2010-03-11 20:44:46 +0000

    code tidy

--- lib/sentry/rights.rb
+++ lib/sentry/rights.rb
@@ -68,76 +68,19 @@ module Sentry
 end
 
 Sentry.rights do
-  
   create do
     new
     create
   end
-  
   read do
     index
     show
   end
-  
   update do
     edit
     update
   end
-  
   delete do
     destroy
   end
-  
-end
-
-
-__END__
-
-module Sentry
-  
-  # TODO: do away with actions and make this a composite pattern??
-  
-  class Right
-    
-    attr_reader :name, :default, :actions
-    
-    def initialize(name, opts)
-      @name = name
-      @default = opts[:default] || false
-      @actions = opts[:actions] || []
-    end
-    
-    def method_name
-      "can_#{name}?"
-    end
-    
-  end
-  
-  class RightsBuilder
-    
-    def initialize(rights)
-      @rights = rights
-    end
-    
-    def method_missing(sym, *args, &block)
-      @rights[sym] = Right.new(sym, args.extract_options!)
-    end
-    
-  end
-  
-  def self.rights(&block)
-    if block_given?
-      Sentry::RightsBuilder.new(@rights = {}).instance_eval(&block)
-    else
-      @rights
-    end
-  end
-  
-end
-
-Sentry.rights do |r|
-  r.create :actions => [:new, :create]
-  r.read :actions => [:index, :show]
-  r.update :actions => [:edit, :update]
-  r.delete :actions => [:destroy]
 end
