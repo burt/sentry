26fe41f3f9972a35691ddbab4cbef9fc28b66ca9
brent <funkingburt@gmail.com>
2010-03-17 12:55:56 +0000

    filter mods

--- lib/sentry/rails.rb
+++ lib/sentry/rails.rb
@@ -1,17 +1,18 @@
 # TODO: rspec
+# TODO: raise bad params
+# TODO: check controllers don't already define methods
 
 module Sentry
   module Rails
     
     def self.included(base)
       base.send :extend, ClassMethods
-      base.rescue_from Sentry::NotAuthorized, :with => :user_not_authorized
+      base.rescue_from Sentry::NotAuthorized, :with => :not_authorized
     end
     
     def initialize
       super
       instance = self
-      # TODO: check the methods aren't already defined 
       Sentry.rights.each do |k, v|
         (class << self; self; end).class_eval do
           define_method(v.method_name) do |model, *args|
@@ -23,7 +24,7 @@ module Sentry
       end
     end
 
-    def user_not_authorized
+    def not_authorized
       flash[:error] = Sentry.configuration.not_permitted_message
       redirect_to Sentry.configuration.not_permitted_redirect.to_s
       return
@@ -47,37 +48,52 @@ module Sentry
         @klass = klass
       end
       
-      def authorize(opts)
-        before_filter(opts.merge!(:authorize => true)) do |sentry, controller|
+      def authorize(*args)
+        before_filter(*args) do |sentry, controller|
+          sentry.options.merge!(:authorize => true)
           sentry.action_permitted?(controller.action_name)
         end
       end
       
-      def filter(opts)
-        before_filter(opts) do |sentry, controller|
+      def filter(*args)
+        before_filter(*args) do |sentry, controller|
           sentry.filter(controller.action_name) if sentry.respond_to? :filter
         end
       end
       
       private
       
-      # TODO: raise if bad params passed
-      def before_filter(opts = {})
+      def before_filter(*args)    
+        opts = prep_filter_options(*args)
         @klass.before_filter(opts) do |controller|
-          model = if opts[:with].is_a?(Proc)
-            controller.instance_eval(&opts[:with])
-          else
-            controller.instance_variable_get("@#{opts[:with]}".to_sym)
-          end 
-          puts "model #{model.inspect}"
+          model = get_model(controller, opts)
           user = controller.sentry_user
           sentry = Sentry.create(model, user, opts)
           yield sentry, controller
         end
       end
       
-      def run_finder(controller, method)
-        controller.send(method, true) 
+      def get_model(controller, opts)
+        model_opt = opts[:with]          
+        model = if model_opt.is_a?(Proc)
+          controller.instance_eval(&model_opt)
+        else
+          controller.instance_variable_get("@#{model_opt}")
+        end
+      end
+
+      def prep_filter_options(*args)
+        opts = args.extract_options!
+        unless args.size == 0
+          actions = if args.include?(:all)
+            [:index, :show, :new, :create, :update, :edit, :delete]
+          else
+            args
+          end
+          opts.merge!(:only => actions)
+          opts.delete(:only) if opts.has_key?(:exclude)
+        end
+        opts
       end
       
     end
