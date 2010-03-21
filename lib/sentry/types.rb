94aac02991e5246c9eae7cba1493d89b897f081c
burt <funkingburt@gmail.com>
2010-03-21 15:44:19 +0000

    member sentry

--- lib/sentry/types.rb
+++ lib/sentry/types.rb
@@ -1,6 +1,8 @@
 class ArraySentry < Sentry::Base
   
   def filter(action, options = {})
+    member_options = options.clone
+    member_options[:sentry] = options[:member_sentry]
     model.reject! do |m|
       !Sentry.create(m, @subject, options).action_permitted?(action)
     end
